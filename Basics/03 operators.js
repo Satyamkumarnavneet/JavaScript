9d039d4dde2e0eb6e386bc484a4efba170c0af19
Satyamkumarnavneet <navneetsatyamkumar@gmail.com>
2023-02-19 04:00:22 +0530

    Committed Operators.js

---
+++ Basics/03 operators.js
@@ -0,0 +1,20 @@
+var a = 6;
+var b = 8;
+
+var sum = a+b;
+console.log(sum);
+
+var isGrater = a > b;
+console.log(isGrater);
+
+
+// Calculate discount percentage
+
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercentage = ((listingPrice - sellingPrice) / listingPrice) * 100;
+console.log("Discount percentage is : " + discountPercentage);
+
+displayDiscountPercentage = Math.round(discountPercentage);
+console.log(displayDiscountPercentage + " % off");
